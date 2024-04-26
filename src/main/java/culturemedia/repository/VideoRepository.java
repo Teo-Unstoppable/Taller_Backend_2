196dd05c7423ed78589499a94d6cf9950a7d3975
Your Name <you@example.com>
2024-04-25 19:35:49 -0500

    Adding video structures

---
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -0,0 +1,14 @@
+package culturemedia.repository;
+
+import java.util.List;
+
+import culturemedia.model.Video;
+
+
+public interface VideoRepository {
+    List<Video> getAllVideos();
+    Video saveVideo(Video save);
+    List<Video> searchByName(String title);
+    List<Video> findByDuration(Double fromDuration, Double toDuration);
+}
+
