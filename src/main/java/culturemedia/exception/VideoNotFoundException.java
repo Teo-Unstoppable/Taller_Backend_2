a41368cf4776974502552a98abeb1df1bce75ad2
Your Name <you@example.com>
2024-04-25 20:30:20 -0500

    Corrections

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -3,9 +3,12 @@ package culturemedia.exception;
 import java.text.MessageFormat;
 
 public class VideoNotFoundException extends CultureMediaException {
-    public VideoNotFoundException(String message, String title) {
-        super(MessageFormat.format("Invalid title for the video {0}", title));
-
+    public VideoNotFoundException() {
+        super("Video not found");
     }
-}
 
+    public VideoNotFoundException(String title) {
+        super(MessageFormat.format("Invalid title for the video {0}", title));
+    }
+
+}
