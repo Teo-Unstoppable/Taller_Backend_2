c284007fbc27acf255adb97bb6e3421ca3008f40
Your Name <you@example.com>
2024-05-14 19:23:22 -0500

    R12-Adding-Components-Rest-API.

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -3,10 +3,19 @@ package culturemedia.controllers;
 import java.util.*;
 
 import culturemedia.exception.CultureMediaException;
+import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
+import culturemedia.repository.Impl.VideoRepositoryImpl;
+import culturemedia.repository.Impl.ViewsRepositoryImpl;
 import culturemedia.service.CultureMediaService;
+import culturemedia.service.Impl.CultureMediaServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 public class CultureMediaController {
     private final CultureMediaService cultureMediaService;
 
@@ -15,9 +24,22 @@ public class CultureMediaController {
         this.cultureMediaService = cultureMediaService;
     }
 
+    public CultureMediaController(){
+        this.cultureMediaService = new CultureMediaServiceImpl(new VideoRepositoryImpl(), new ViewsRepositoryImpl());
+    }
 
+    @GetMapping
     public List<Video> findAllVideos() throws CultureMediaException {
-        return cultureMediaService.findAll();
+        try {
+            return cultureMediaService.findAll();
+        }catch (VideoNotFoundException e){
+            return Collections.emptyList();
+        }
+    }
+
+    @PostMapping
+    public Video addVideo(@RequestBody Video video){
+        return cultureMediaService.save(video);
     }
 }
 
